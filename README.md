cd82a1c4f7a6f6e490a918ae07f6e582698f0dbc
Steve Winton <stevewinton@gmail.com>
2019-05-15 11:31:02 -0500

    Use nock.back, add README, etc

---
+++ README.md
@@ -0,0 +1,17 @@
+# `nock.back.example`
+
+> Example of `nock.back` using the GitHub API
+
+## Usage
+
+```bash
+# install dependencies
+npm i .
+
+# run the tests
+npm t
+```
+
+## References
+
+- [Mocking HTTP requests with Nock](https://codeburst.io/testing-mocking-http-requests-with-nock-480e3f164851)
--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,38 @@
+const path = require('path');
 const nock = require('nock')
-const getLogin = require('..')
+let getLogin
 
-test('it returns the authenticated user\'s login', async () => {
-  nock.back.setMode('record')
+describe('getLogin against GitHub.com', () => {
+  beforeAll(() => {
+    jest.resetModules()
+    getLogin = require('..')
+    nock.back.fixtures = path.join(__dirname, '..', 'fixtures', 'dotcom')
+  })
 
-  nock('https://api.github.com')
-    .get('/user')
-    .reply(200, {
-      login: 'octocat'
-    })
-  
-  const login = await getLogin()
+  beforeEach(() => {
+    nock.back.setMode('record')
+  })
 
-  expect(login).toBe('octocat')
+  afterEach(() => {
+    nock.back.setMode('wild')
+    nock.cleanAll()
+  })
 
-  nock.back.setMode('wild')
+  afterAll(nock.restore)
+
+  test('it returns the authenticated user\'s login', async () => {  
+    const { nockDone } = await nock.back(
+      'get-authenticated-user.json'
+    )
+
+    const login = await getLogin()
+
+    expect(login).toBe('swinton')
+
+    nockDone()
+  })
+})
+
+describe('getLogin against GitHub Enterprise Server', () => {
+  // TODO
 })
